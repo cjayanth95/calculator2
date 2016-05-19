68c971ec2100ac7d7449e8061e8e57988562ec9c
Jayanth <cjayanth95@gmail.com>
2016-05-19 11:50:48 +0530

    Basic version

--- calculator2/ViewController.h
+++ calculator2/ViewController.h
@@ -8,7 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    bool operatorpressed;
+    char add;
+    NSString * first;
+    NSString * second;
+    
+}
+
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+- (IBAction)zero:(id)sender;
+- (IBAction)minus:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)clear:(id)sender;
+- (IBAction)equals:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *answer;
 
 
 @end
--- calculator2/ViewController.m
+++ calculator2/ViewController.m
@@ -1,8 +1,8 @@
 //
 //  ViewController.m
-//  calculator2
+//  Calculator
 //
-//  Created by labuser on 19/05/16.
+//  Created by labuser on 18/05/16.
 //  Copyright © 2016 labuser. All rights reserved.
 //
 
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    operatorpressed = FALSE;
+    first = NULL;
+    second = NULL;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +27,269 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)equals:(id)sender {
+    if (add == false) {
+        double num = [first doubleValue] - [second doubleValue];
+        _answer.text = [NSString stringWithFormat:@"%f", num];
+        
+    }
+    else{
+        double num = [first doubleValue] + [second doubleValue];
+        _answer.text = [NSString stringWithFormat:@"%f", num];
+    }
+    operatorpressed = FALSE;
+    first = NULL;
+    second = NULL;
+}
+- (IBAction)clear:(id)sender {
+    operatorpressed = FALSE;
+    first = NULL;
+    second = NULL;
+    _answer.text = NULL;
+}
+
+
+
+- (IBAction)one:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,1];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 1];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,1];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 1];
+            _answer.text = second;
+        }
+    }
+}
+- (IBAction)two:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,2];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 2];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,2];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 2];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)three:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,3];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 3];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,3];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 3];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)four:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,4];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 4];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,4];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 4];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)five:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,5];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 5];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,5];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 5];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)six:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,6];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 6];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,6];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 6];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)seven:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,7];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 7];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,7];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 7];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)eight:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,8];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 8];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,8];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 8];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)nine:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,9];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 9];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,9];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 9];
+            _answer.text = second;
+        }
+    }
+}
+
+- (IBAction)zero:(id)sender {
+    if (operatorpressed == FALSE) {
+        if (first == NULL) {
+            first = [NSString stringWithFormat:@"%i" ,0];
+            _answer.text = first;
+        }
+        else{
+            first = [NSString stringWithFormat:@"%@%i" ,first, 0];
+             _answer.text = first;
+        }
+    }
+    else{
+        if (second == NULL) {
+            second = [NSString stringWithFormat:@"%i" ,0];
+            _answer.text = second;
+        }
+        else{
+            second = [NSString stringWithFormat:@"%@%i" ,second, 0];
+            _answer.text = second;
+        }
+    }
+}
+
+
+- (IBAction)plus:(id)sender {
+    add = TRUE;
+    operatorpressed = TRUE;
+}
+
+- (IBAction)minus:(id)sender {
+    add = FALSE;
+    operatorpressed = TRUE;
+}
+
 @end
+
+
